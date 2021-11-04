7e6122c271e6345fd3b1b0c3a35006ac021fdf1a
difeorqui <dfoq76@hotmail.com>
2021-11-03 20:56:02 -0500

    Se crea base para revisiones y fallas

---
+++ te-sys/src/pages/Failure.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../componentes/NavBar';
+
+const Failure = () => {
+    return (  
+        <div className="mt-5 p-5 text-black"><h1>REPORTE DE FALLAS</h1>
+        <NavBar/>
+    </div>
+
+    );
+}
+ 
+export default Failure;
---
+++ te-sys/src/pages/Review.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../componentes/NavBar';
+
+const Review = () => {
+    return (  
+        <div className="mt-5 p-5 text-black"><h1>REVISIONES</h1>
+        <NavBar/>
+    </div>
+
+    );
+}
+ 
+export default Review;
