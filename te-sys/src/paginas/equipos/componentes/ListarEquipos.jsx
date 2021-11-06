dc198f486b6ee19c13d9bbd9be3418685a94022b
YuirQuintero5 <ingenieraquintero@gmail.com>
2021-11-05 20:29:19 -0500

    Crud Equipos

--- te-sys/src/paginas/equipos/componentes/ListarEquipos.jsx
+++ te-sys/src/paginas/equipos/componentes/ListarEquipos.jsx
@@ -1,75 +1,53 @@
-import Imagenes from './Imagenes';
-
 const ListarEquipos = () => {
-    return(
-        <div className="container">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Foto</th>
-                        <th scope="col">Fecha</th>
-                        <th scope="col">Número de serie</th>
-                        <th scope="col">Nombre de equipo</th>
-                        <th scope="col">Marca</th>
-                        <th scope="col">Fabricante</th>
-                        <th scope="col">Estado</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td><img className="img-equipo" src={Imagenes.img1} alt="" width="30" height="30"></img></td>
-                        <td></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td>
-                        <select class="form-select w-auto" aria-label="Default select example">
-                            <option selected>Estado</option>
-                            <option value="1">Activo</option>
-                            <option value="2">Inactivo</option>
-                            <option value="3">En mantenimiento</option>
-                        </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td><td><img className="img-equipo" src={Imagenes.img2} alt="" width="30" height="30"></img></td></td>
-                        <td></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><select class="form-select w-auto" aria-label="Default select example">
-                            <option selected>Estado</option>
-                            <option value="1">Activo</option>
-                            <option value="2">Inactivo</option>
-                            <option value="3">En mantenimiento</option>
-                        </select></td>                        
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td><img className="img-equipo" src={Imagenes.img4} alt="" width="30" height="30"></img></td>
-                        <td></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><input type="text" class="form-control w-50" id="exampleFormControlInput1" placeholder=""></input></td>
-                        <td><select class="form-select w-auto" aria-label="Default select example">
-                            <option selected>Estado</option>
-                            <option value="1">Activo</option>
-                            <option value="2">Inactivo</option>
-                            <option value="3">En mantenimiento</option>
-                        </select></td>
-                        
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-
-}
-
-export default ListarEquipos;
+    return (
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+            <th scope="col">Imagen</th>  
+              <th scope="col">Serial</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Descripcion</th>
+              <th scope="col">Marca</th>
+              <th scope="col">Estado</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <i class="fas fa-eye"></i>
+              </td>
+              <td>Serial 1</td>
+              <td>Equipo 1</td>
+              <td>Descripcion 1</td>
+              <td>Marca 1</td>
+              <td>Estado 1</td>
+            </tr>
+           <tr>
+              <td>
+                <i class="fas fa-eye"></i>
+              </td>
+              <td>Serial 2</td>
+              <td>Equipo 2</td>
+              <td>Descripcion 2</td>
+              <td>Marca 2</td>
+              <td>Estado 2</td>
+            </tr>
+            <tr>
+              <td>
+                <i class="fas fa-eye"></i>
+              </td>
+              <td>Serial 3</td>
+              <td>Equipo 3</td>
+              <td>Descripcion 3</td>
+              <td>Marca 3</td>
+              <td>Estado 3</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default ListarEquipos;
+  
