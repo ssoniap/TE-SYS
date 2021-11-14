13750ce8f08553702e7c5acb72db1bd2be31f7fd
AndresBaez96 <afbaezm@unal.edu.co>
2021-11-13 21:42:56 -0500

    Listas en creación de usuarios mejorada

--- te-sys/src/paginas/user/components/createUser.jsx
+++ te-sys/src/paginas/user/components/createUser.jsx
@@ -14,7 +14,7 @@ const Form =() => {
                       placeholder="Escriba el nombre de usuario"
                       required
                       />
-                      <label for="floatingName">Nombre de Usuario</label>
+                      <label htmlFor="floatingName">Nombre de Usuario</label>
                   </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-5">
@@ -30,20 +30,32 @@ const Form =() => {
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-5">
-                <select className="form-select form-select-lg mb-3">
-                    <option selected disabled>Rol asignado al usuario</option>
-                    <option>Rol 1</option>
-                    <option>Rol 2</option>
-                    <option>Rol 3</option>
-                  </select>
+                  <div className="form-floating mb-3">
+                    <select
+                      className="form-select"
+                      id="floatingList1"
+                      required
+                    >
+                      <option>Rol 1</option>
+                      <option>Rol 2</option>
+                      <option>Rol 3</option>
+                    </select>
+                    <label htmlFor="floatingList1">Lista de roles</label>
+                  </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-5">
-                  <select className="form-select form-select-lg mb-3">
-                    <option selected disabled>Tercero asignado al usuario</option>
-                    <option>Tercero 1</option>
-                    <option>Tercero 2</option>
-                    <option>Tercero 3</option>
-                  </select>
+                  <div className="form-floating mb-3">
+                    <select
+                      className="form-select"
+                      id="floatingList2"
+                      required
+                    >
+                      <option>Tercero 1</option>
+                      <option>Tercero 2</option>
+                      <option>Tercero 3</option>
+                    </select>
+                    <label htmlFor="floatingList2">Lista de terceros</label>
+                  </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-5">
                   <button type="button" class="btn btn-primary mx-1">Agregar usuario</button>
