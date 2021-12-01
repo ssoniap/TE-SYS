a30c5eaa892cfe3f4de9ee0291597b25cb49daec
YuriQuintero5 <65046429+YuriQuintero5@users.noreply.github.com>
2021-11-30 21:09:43 -0500

    cAMBIOS EN EQUIPO

--- te-sys/src/paginas/equipos/components/AssignEquipmentList.jsx
+++ te-sys/src/paginas/equipos/components/AssignEquipmentList.jsx
@@ -1,60 +1,55 @@
+import { useEffect, useState } from "react";
+import apiEquipment from "../../../services/apiEquipment";
+
+import FilasAssignList from "./FilasAssignList";
+
+
 const ListarEquiposAsignar = () => {
+
+  const [machines, setMachines] = useState([]);
+
+  const getEquipments = async () => {
+    const response = await apiEquipment.getEquipments();
+    setMachines(response.data.data);
+  };
+
+  useEffect(() => {
+    getEquipments();
+  }, []);
+  
+
+
     return (
       <div className="table-responsive">
         <table className="table table-striped table-bordered">
           <thead>
-            <tr>
-              <th scope="col">Asignar</th>
+            <tr>              
               <th scope="col">Imagen</th>  
               <th scope="col">Serial</th>
               <th scope="col">Nombre</th>
               <th scope="col">Descripcion</th>
               <th scope="col">Estado</th>
+              <th scope="col">Asignar</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-            <td>
-            <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="check1"></input>
-            </div>
-            </td> 
-            <td>
-                <i class="fas fa-eye"></i>
-              </td>
-              <td>Serial 1</td>
-              <td>Equipo 1</td>
-              <td>Descripcion 1</td>
-              <td>Estado 1</td>
-            </tr>
-            <tr>
-            <td>
-            <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="check2"></input>
-            </div>
-            </td>  
-            <td>
-              <i class="fas fa-eye"></i>
-            </td>
-            <td>Serial 2</td>
-            <td>Equipo 2</td>
-            <td>Descripcion 2</td>
-            <td>Estado 2</td>
-          </tr>
-          <tr>
-          <td>
-            <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="check3"></input>
-            </div>
-            </td>
-            <td>
-              <i class="fas fa-eye"></i>
-            </td>
-            <td>Serial 3</td>
-            <td>Equipo 3</td>
-            <td>Descripcion 3</td>
-            <td>Estado 3</td>
-          </tr>
+          {machines &&
+          machines.map((filasAssignList) => {
+              return (
+                <FilasAssignList
+                  key={filasAssignList.id}
+                  info={{                    
+                    picture: filasAssignList.picture,
+                    serial: filasAssignList.serial, 
+                    machineName: filasAssignList.machineName,
+                    description: filasAssignList.description,
+                    status: filasAssignList.status,
+                    
+                  }}                  
+                />
+              );
+            })}
+                   
           </tbody>
         </table>
       </div>
